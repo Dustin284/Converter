a2012a6b792001a1eaaa8acdbe23e9093ce3b303
Dustin-Joel Eden <dustin.joel.eden@gmail.com>
2023-05-23 21:38:50 +0200

    Projektdateien hinzufügen.

---
+++ Converter_Umrechner/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Converter_Umrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Umrechner = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.geschwindigkeitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Umrechner
+            // 
+            this.Umrechner.Name = "Umrechner";
+            this.Umrechner.Size = new System.Drawing.Size(61, 4);
+            this.Umrechner.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.geschwindigkeitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(239, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // geschwindigkeitToolStripMenuItem
+            // 
+            this.geschwindigkeitToolStripMenuItem.Name = "geschwindigkeitToolStripMenuItem";
+            this.geschwindigkeitToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.geschwindigkeitToolStripMenuItem.Text = "Geschwindigkeit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Umrechnen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 82);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(77, 18);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mp/h",
+            "ft/s",
+            "m/s",
+            "km/h",
+            "kn"});
+            this.comboBox1.Location = new System.Drawing.Point(95, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(60, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox2.Location = new System.Drawing.Point(12, 106);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(77, 18);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox3.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox3.Multiline = false;
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(77, 18);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox4.Location = new System.Drawing.Point(12, 154);
+            this.richTextBox4.Multiline = false;
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(77, 18);
+            this.richTextBox4.TabIndex = 10;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox5.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox5.Location = new System.Drawing.Point(12, 178);
+            this.richTextBox5.Multiline = false;
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.Size = new System.Drawing.Size(77, 18);
+            this.richTextBox5.TabIndex = 11;
+            this.richTextBox5.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 205);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip Umrechner;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem geschwindigkeitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+    }
+}
+
---
+++ Converter_Umrechner/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter_Umrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clear_text();
+            if (textBox1.TextLength == 0) { MessageBox.Show("Bitte gebe eine Zahlen an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            else if (double.TryParse(textBox1.Text, out double result)) { CheckSelectedElement();  }
+            else { MessageBox.Show("Bitte gebe eine Zahlen an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void CheckSelectedElement()
+        {
+            // Überprüfe, ob ein Element ausgewählt ist
+            if (comboBox1.SelectedIndex != -1)
+            {
+                // Hole das ausgewählte Element
+                string selectedElement = comboBox1.SelectedItem.ToString();
+                double num1 = Convert.ToDouble(textBox1.Text);
+
+                if (selectedElement == "mp/h") { 
+                    String str1 = textBox1.Text + " mp/h";
+                    richTextBox1.AppendText(str1); 
+
+                    double mp_h_to_ft_s = (double)1.467; 
+                    double mp_h_to_ft_s_result = (double)num1 * mp_h_to_ft_s;
+                    String str2 = Math.Round(mp_h_to_ft_s_result, 2) + " ft/s";
+                    richTextBox2.AppendText(str2);
+
+
+                    double mp_h_to_m_s = (double)2.237;
+                    double mp_h_to_m_s_result = (double)num1 / mp_h_to_m_s;
+                    String str3 = Math.Round(mp_h_to_m_s_result, 2) + " m/s";
+                    richTextBox3.AppendText(str3);
+
+                    double mp_h_to_km_h = (double)1.609;
+                    double mp_h_to_km_h_result = (double)num1 * mp_h_to_km_h;
+                    String str4 = Math.Round(mp_h_to_km_h_result, 3) + " km/h";
+                    richTextBox4.AppendText(str4);
+
+                    double mp_h_to_knoten = (double)1.151;
+                    double mp_h_to_knoten_result = (double)num1 / mp_h_to_knoten;
+                    String str5 = Math.Round(mp_h_to_knoten_result, 3) + " kn";
+                    richTextBox5.AppendText(str5);
+
+
+
+                }
+                else if (selectedElement == "ft/s") {
+                    
+                    double ft_s_to_m_ph = (double)1.467;
+                    double ft_s_to_m_ph_result = (double)num1 / ft_s_to_m_ph;
+                    String str1 = Math.Round(ft_s_to_m_ph_result, 2) + " ft/s";
+                    richTextBox1.AppendText(str1);
+
+             
+                    String str2 = textBox1.Text + " ft/s";
+                    richTextBox2.AppendText(str2);
+
+
+                    double ft_s_to_m_s = (double)3.281;
+                    double ft_s_to_m_s_result = (double)num1 / ft_s_to_m_s;
+                    String str3 = Math.Round(ft_s_to_m_s_result, 2) + " m/s";
+                    richTextBox3.AppendText(str3);
+
+                    double ft_s_to_km_h = (double)1.097;
+                    double ft_s_to_km_h_result = (double)num1 * ft_s_to_km_h;
+                    String str4 = Math.Round(ft_s_to_km_h_result, 3) + " km/h";
+                    richTextBox4.AppendText(str4);
+
+                    double ft_s_to_knoten = (double)1.688;
+                    double ft_s_to_knoten_result = (double)num1 / ft_s_to_knoten;
+                    String str5 = Math.Round(ft_s_to_knoten_result, 3) + " kn";
+                    richTextBox5.AppendText(str5);
+
+
+                }
+                else if (selectedElement == "m/s") {
+                   cooming_soon();
+                }
+                else if (selectedElement == "km/h") { 
+                    cooming_soon();
+                }
+                else if (selectedElement == "kn") { 
+                    cooming_soon();
+                }
+            }
+            else
+            {
+                // Kein Element ausgewählt
+                MessageBox.Show("Kein Element ausgewählt.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_text()
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            richTextBox5.Clear();
+        }
+
+        private void cooming_soon()
+        {
+            MessageBox.Show("Dieses Feature ist noch nicht verfügbar.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+    }
+}
